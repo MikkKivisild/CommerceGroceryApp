13f0b045343d00997d5ad224435ea543b08967e8
askatskov <160420092+askatskov@users.noreply.github.com>
2025-03-10 13:26:39 +0200

    Part 2

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -16,8 +16,8 @@ namespace CommerceGroceryApp
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Ubuntu-Regular.ttf", "Ubuntu");
+                    fonts.AddFont("Ubuntu-Bold.ttf", "UbuntuBold");
                 })
                 .UseMauiCommunityToolkit();
             builder.Services.AddSingleton<CategoryService>();
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CommerceGroceryApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CommerceGroceryApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommerceGroceryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+24eb9822199b4f4cade4f28568fa8c50d9a43964")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0aa4c554015484de75f36297b02ceeccd39bb4c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommerceGroceryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommerceGroceryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
